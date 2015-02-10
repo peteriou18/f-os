f38a086fa895818c257627a6b9350639d69e1db1
vpn288 <vpn@vden.ru>
2015-02-11 00:04:07 +0300

    Update pm-rm.asm

--- 32bit/init/pm-rm.asm
+++ 32bit/init/pm-rm.asm
@@ -2,41 +2,45 @@
 
 switch_to_pm:
          cli
+
          mov eax, cr0
          or al, 0x01     ; Set protected mode bit
          mov cr0, eax
+         wbinvd
          jmp 8:pm2
 
          USE32
-          mov dword [0x000B8030], "I S "
+ esp_save       dd       0
  pm2:
-        lidt    [idtr]
+
+
         mov eax, 16     ; load 4 GB data descriptor
         mov ds, ax      ; to all data segment registers
         mov es, ax
         mov fs, ax
         mov gs, ax
         mov ss, ax
-        xor eax, eax
-        xor ebx, ebx
-        xor ecx, ecx
-        xor edx, edx
-        xor esi, esi
-        xor edi, edi
-        xor ebp, ebp
-        mov esp,08000h
-        call     remap_irq_pm
-        call    unmask_irqs
+       ; mov esp,05000h
+        lidt    [idtr]
+   ;    call     remap_irq_pm
+   ;     call    unmask_irqs
+        mov    esp,0f00h-36
+        popad
+        mov     esp,[esp_save]
         sti
-        jmp     far [pmback]
+
+        jmp     dword [pmback]
 
 
 switch_to_rm:
          cli
 
+         mov     [esp_save],esp
+         mov    esp,0f00h
+         pushad
          jmp    30h:rm3
          nop
-         USE16
+             USE16
  rm3:
         mov   eax,38h
         mov     ds,ax
@@ -49,11 +53,12 @@ switch_to_rm:
         and al, 0xfe
             ; clear protected mode bit
          mov cr0, eax
+         wbinvd
          jmp    0:start163
 
 
 
-          
+
  start163:
 
           xor     ax,ax
@@ -63,13 +68,13 @@ switch_to_rm:
           mov     fs,ax
 
           mov     gs,ax
-          mov     ax,0b800h
+       ;   mov     ax,0b800h
           mov     fs,ax
-          mov     sp,8000h
-          call  remap_irq_real
+          mov     sp,5000h
+       ;   call  remap_irq_real
           lidt     [rlidt]
-          sti
-          call  unmask_irqs
+        ;  sti
+        ;  call  unmask_irqs
 
           jmp   far [rmback]
 
