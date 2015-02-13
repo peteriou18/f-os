d8da71b9eb464f5ac9c25ba9a479cd4e81389e35
vpn288 <vpn@vden.ru>
2015-02-14 00:04:07 +0300

    Update screen.asm

--- 32bit/screen.asm
+++ 32bit/screen.asm
@@ -18,26 +18,26 @@ os_inc_cursor:
         push eax
 
         add word [os_Screen_Cursor_Col], 1
-        mov ax, [os_Screen_Cursor_Col]
+        mov al, [os_Screen_Cursor_Col]
         cmp ax, [os_Screen_Cols]
-        jne os_inc_cursor_done
-        mov word [os_Screen_Cursor_Col], 0
-        add word [os_Screen_Cursor_Row], 1
-        mov ax, [os_Screen_Cursor_Row]
+        jnl os_inc_cursor_done
+        mov byte [os_Screen_Cursor_Col], 0
+        add byte [os_Screen_Cursor_Row], 1
+        mov al, [os_Screen_Cursor_Row]
         cmp ax, [os_Screen_Rows]
-        jne os_inc_cursor_done
-        call os_screen_scroll
-        sub word [os_Screen_Cursor_Row], 1
+        jnl os_inc_cursor_done
+;        call os_screen_scroll
+        sub byte [os_Screen_Cursor_Row], 1
 
 os_inc_cursor_done:
         call    os_cursor_set
         pop eax
         ret
 ; -----------------------------------------------------------------------------
-os_Screen_Cursor_Col    dw      0
-os_Screen_Cols                  dw      80
+os_Screen_Cursor_Col    db      0
+os_Screen_Cols                  dw      20 ;40 ;80
 
-os_Screen_Cursor_Row    dw      0
+os_Screen_Cursor_Row    db      0
 os_Screen_Rows                  dw      25
 ; -----------------------------------------------------------------------------
 ; os_dec_cursor -- Decrement the cursor by one
@@ -46,14 +46,14 @@ os_Screen_Rows                  dw      25
 os_dec_cursor:
         push eax
 
-        cmp word [os_Screen_Cursor_Col], 0
+        cmp byte [os_Screen_Cursor_Col], 0
         jne os_dec_cursor_done
-        sub word [os_Screen_Cursor_Row], 1
+        sub byte [os_Screen_Cursor_Row], 1
         mov ax, [os_Screen_Cols]
-        mov word [os_Screen_Cursor_Col], ax
+        mov  [os_Screen_Cursor_Col], al
 
 os_dec_cursor_done:
-        sub word [os_Screen_Cursor_Col], 1
+        sub byte [os_Screen_Cursor_Col], 1
         call     os_cursor_set
         pop eax
         ret
@@ -62,13 +62,25 @@ os_dec_cursor_done:
 os_cursor_set:
         push    edx
         push    eax
+        push    ebx
+        xor     ebx,ebx
+        xor     eax,eax
+        mov     bl,byte [os_Screen_Cursor_Row]
+        lea     ebx,[ebx+ebx*4]
+        shl     ebx,4
+        mov     al,byte [os_Screen_Cursor_Col]
+        add     ebx,eax
+
         mov     edx,3d4h
         mov     al,0fh
-        mov     ah,byte [os_Screen_Cursor_Col]
+        mov     ah,bl
         out     dx,ax
         mov     al,0eh
-        mov     ah,byte [os_Screen_Cursor_Row]
+
+        mov     edx,3d4h
+        mov     ah,bh
         out     dx,ax
+        pop     ebx
         pop     eax
         pop     edx
         ret
@@ -76,19 +88,19 @@ os_cursor_set:
 ; os_print_newline -- Reset cursor to start of next line and scroll if needed
 ;  IN:  Nothing
 ; OUT:  All registers perserved
-os_print_newline:
+;os_print_newline:
         push eax
 
         mov word [os_Screen_Cursor_Col], 0
         mov ax, [os_Screen_Rows]
         sub ax, 1
-        cmp ax, [os_Screen_Cursor_Row]
+        cmp al, [os_Screen_Cursor_Row]
         je os_print_newline_scroll
-        add word [os_Screen_Cursor_Row], 1
+        add byte [os_Screen_Cursor_Row], 1
         jmp os_print_newline_done
 
 os_print_newline_scroll:
-        call os_screen_scroll
+;        call os_screen_scroll
 
 os_print_newline_done:
         pop eax
@@ -100,14 +112,43 @@ os_print_newline_done:
 ; os_output -- Displays text
 ;  IN:  RSI = message location (zero-terminated string)
 ; OUT:  All registers perserved
+  USE32
 os_output:
-        push ecx
+        pushad
+        mov     eax,prtstr1
+        and     eax,0ffffh
 
-        call os_string_length
-        call os_output_chars
+        mov     [rmback],eax
 
-        pop ecx
+
+        mov     dword [pmback_offset],prtstr2
+        jmp    switch_to_rm
+
+        USE16
+        cld
+        cli
+prtstr1:
+
+        ;lodsb
+        mov     al,[si]
+        inc     si
+        cmp    al,0
+        je      prtstr3
+        mov     ah,0eh
+        int     10h
+        jmp     prtstr1
+ prtstr3:
+         ; call    print_string_16
+
+        jmp   switch_to_pm
+
+        USE32
+prtstr2:
+
+        popad
         ret
+ symbols        dd      0
+
 ; -----------------------------------------------------------------------------
 
 
@@ -116,336 +157,84 @@ os_output:
 ;  IN:  AL  = char to display
 ; OUT:  All registers perserved
 os_output_char:
-        push edi
-        push edx
-        push ecx
-        push ebx
-        push eax
+        pusha
 
-        ;cmp byte [os_VideoEnabled], 1
-        ;je os_output_char_graphics
+        mov     [out_char],al
+        mov     word [rmback], prtchr1
+        mov     dword [pmback_offset],prtchr2
+        jmp    switch_to_rm
 
-os_output_char_text:
-        mov ah, [screen_attribute] ;0x07            ; Store the attribute into AH so STOSW can be used later on
+        USE16
+prtchr1:
+         cli
+         mov    ah,0eh
+         mov    al,[out_char]
+         mov    cx,4
+         xor    bh,bh
+         mov    bl,[ screen_attribute]
+         int    10h
 
-        push eax
-        mov ax, [os_Screen_Cursor_Row]
-        and eax, 0x000000000000FFFF     ; only keep the low 16 bits
-        mov cl, 80                      ; 80 columns per row
-        mul cl                          ; AX = AL * CL
-        mov bx, [os_Screen_Cursor_Col]
-        add ax, bx
-        shl ax, 1                       ; multiply by 2
-        mov edi, 0xB8000                ; Address of the screen buffer
-        add edi, eax
-        pop eax
+        jmp   switch_to_pm
 
-        stosw                   ; Write the character and attribute with one call
-        jmp os_output_char_done
+        USE32
+prtchr2:
 
-os_output_char_graphics:
-;       mov ebx, [os_Font_Color]
-        and eax, 0x000000FF
-        call os_glyph_put
-
-os_output_char_done:
-        call os_inc_cursor
-
-        pop eax
-        pop ebx
-        pop ecx
-        pop edx
-        pop edi
+        popa
         ret
+
+ out_char       db      0
  screen_attribute       db      7
 ; -----------------------------------------------------------------------------
-
-
-; -----------------------------------------------------------------------------
-; os_pixel -- Put a pixel on the screen
-;  IN:  EBX = Packed X & Y coordinates (YYYYXXXX)
-;       EAX = Pixel Details (AARRGGBB)
-; OUT:  All registers preserved
-os_pixel:
-        push edi
-        push edx
-        push ecx
-        push ebx
-        push eax
-
-        push eax                        ; Save the pixel details
-        mov eax, ebx
-        shr eax, 16                     ; Isolate Y coord
-        xor ecx, ecx
-        ;mov cx, [os_VideoX]
-        mul ecx                         ; Multiply Y by os_VideoX
-        and ebx, 0x0000FFFF             ; Isolate X coord
-        add eax, ebx                    ; Add X
-        ;mov rdi, [os_VideoBase]
-
-        ;cmp byte [os_VideoDepth], 32
-        ;je os_pixel_32
-
-os_pixel_24:
-        mov ecx, 3
-        mul ecx                         ; Multiply by 3 as each pixel is 3 bytes
-        add edi, eax                    ; Add offset to pixel video memory
-        pop eax                         ; Restore pixel details
-        stosb
-        shr eax, 8
-        stosb
-        shr eax, 8
-        stosb
-        jmp os_pixel_done
-
-os_pixel_32:
-        shl eax, 2                      ; Quickly multiply by 4
-        add edi, eax                    ; Add offset to pixel video memory
-        pop eax                         ; Restore pixel details
-        stosd
-
-os_pixel_done:
-        pop eax
-        pop ebx
-        pop ecx
-        pop edx
-        pop edi
-        ret
-; -----------------------------------------------------------------------------
-
-
-; -----------------------------------------------------------------------------
-; os_glyph_put -- Put a glyph on the screen at the cursor location
-;  IN:  EAX = Glyph
-;       EBX = Color (AARRGGBB)
-; OUT:  All registers preserved
-os_glyph_put:
-        push edi
-        push esi
-        push edx
-        push ecx
-        push ebx
-        push eax
-
-        and eax, 0x000000FF
-        sub eax, 0x20
-        mov ecx, 12             ; Font height
-        mul ecx
-        ;mov rsi, font_data
-        add esi, eax            ; add offset to correct glyph
-
-; Calculate pixel co-ords for character
-        xor ebx, ebx
-        xor edx, edx
-        xor eax, eax
-        mov ax, [os_Screen_Cursor_Row]
-        mov cx, 12              ; Font height
-        mul cx
-        mov bx, ax
-        shl ebx, 16
-        xor edx, edx
-        xor eax, eax
-        mov ax, [os_Screen_Cursor_Col]
-        mov cx, 6               ; Font width
-        mul cx
-        mov bx, ax
-
-        xor eax, eax
-        xor ecx, ecx            ; x counter
-        xor edx, edx            ; y counter
-
-nextline1:
-        lodsb                   ; Load a line
-
-nextpixel:
-        cmp ecx, 6              ; Font width
-        je bailout              ; Glyph row complete
-        rol al, 1
-        bt ax, 0
-        jc os_glyph_put_pixel
-        push eax
-        mov eax, 0x00000000
-        call os_pixel
-        pop eax
-        jmp os_glyph_put_skip
-
-os_glyph_put_pixel:
-        push eax
-        ;mov eax, [os_Font_Color]
-        call os_pixel
-        pop eax
-os_glyph_put_skip:
-        add ebx, 1
-        add ecx, 1
-        jmp nextpixel
-
-bailout:
-        xor ecx, ecx
-        sub ebx, 6              ; column start
-        add ebx, 0x00010000     ; next row
-        add edx, 1
-        cmp edx, 12             ; Font height
-        jne nextline1
-
-glyph_done:
-        pop eax
-        pop ebx
-        pop ecx
-        pop edx
-        pop esi
-        pop edi
-        ret
-; -----------------------------------------------------------------------------
-
 
 ; -----------------------------------------------------------------------------
 ; os_output_chars -- Displays text
 ;  IN:  RSI = message location (A string, not zero-terminated)
 ;       RCX = number of chars to print
 ; OUT:  All registers perserved
+  USE32
 os_output_chars:
-        push edi
-        push esi
-        push ecx
-        push eax
+        pusha
+        mov     [out_chars],esi
+        mov     [n_chars],ecx
+        mov     dword [rmback], outchr1
+        mov     dword [pmback_offset],outchr2
+        jmp    switch_to_rm
 
-        cld                             ; Clear the direction flag.. we want to increment through the string
-        mov ah, 0x07                    ; Store the attribute into AH so STOSW can be used later on
+        USE16
+outchr1:
+cli
+         cld
+          xor   ax,ax
+          mov   ds,ax
+         mov    ecx,[n_chars]
+;         xor    ecx,0ffffh
+; mov     dword [1000h],32343739H
+         mov    esi,[out_chars]; [1000h];hex_byte
+       ;  mov    ecx,2
+outchr3:
+      ;  push    cx
+         mov    ah,0eh
+         xor    bx,bx
+         ;lodsb
+         mov    al,[si]
+         int    10h
+         inc    si
+       ;  dec    cx
+         ;pop    cx
+         loop    outchr3
 
-os_output_chars_nextchar:
-        jcxz os_output_chars_done
-        sub ecx, 1
-        lodsb                           ; Get char from string and store in AL
-        cmp al, 13                      ; Check if there was a newline character in the string
-        je os_output_chars_newline      ; If so then we print a new line
-        cmp al, 10                      ; Check if there was a newline character in the string
-        je os_output_chars_newline      ; If so then we print a new line
-        cmp al, 9
-        je os_output_chars_tab
-        call os_output_char
-        jmp os_output_chars_nextchar
+        jmp   switch_to_pm
 
-os_output_chars_newline:
-        mov al, [esi]
-        cmp al, 10
-        je os_output_chars_newline_skip_LF
-        call os_print_newline
-        jmp os_output_chars_nextchar
+ out_chars:    dw        0,0
+ n_chars:      dw       5,0
+        USE32
+outchr2:
 
-os_output_chars_newline_skip_LF:
-        cmp ecx, 0
-        je os_output_chars_newline_skip_LF_nosub
-        sub ecx, 1
-os_output_chars_newline_skip_LF_nosub:
-        add esi, 1
-        call os_print_newline
-        jmp os_output_chars_nextchar    
-
-os_output_chars_tab:
-        push ecx
-        mov ax, [os_Screen_Cursor_Col]  ; Grab the current cursor X value (ex 7)
-        mov cx, ax
-        add ax, 8                       ; Add 8 (ex 15)
-        shr ax, 3                       ; Clear lowest 3 bits (ex 8)
-        shl ax, 3                       ; Bug? 'xor al, 7' doesn't work...
-        sub ax, cx                      ; (ex 8 - 7 = 1)
-        mov cx, ax
-        mov al, ' '
-os_output_chars_tab_next:
-        call os_output_char
-        sub cx, 1
-        cmp cx, 0
-        jne os_output_chars_tab_next
-        pop ecx
-        jmp os_output_chars_nextchar
-
-os_output_chars_done:
-        pop eax
-        pop ecx
-        pop esi
-        pop edi
+        popa
         ret
-; -----------------------------------------------------------------------------
-
 
 ; -----------------------------------------------------------------------------
-; os_scroll_screen -- Scrolls the screen up by one line
-;  IN:  Nothing
-; OUT:  All registers perserved
-os_screen_scroll:
-        push esi
-        push edi
-        push ecx
-        push eax
 
-        cld                             ; Clear the direction flag as we want to increment through memory
-
-        ;cmp byte [os_VideoEnabled], 1
-        ;je os_screen_scroll_graphics
-
-os_screen_scroll_text:
-        mov esi, 0xB80A0                ; Start of video text memory for row 2
-        mov edi, 0xB8000                ; Start of video text memory for row 1
-        mov ecx, 1920                   ; 80 x 24
-        rep movsw                       ; Copy the Character and Attribute
-        ; Clear the last line in video memory
-        mov ax, 0x0720                  ; 0x07 for black background/white foreground, 0x20 for space (black) character
-        mov edi, 0xB8F00
-        mov ecx, 80
-        rep stosw                       ; Store word in AX to RDI, RCX times
-        jmp os_screen_scroll_done
-
-os_screen_scroll_graphics:
-        xor esi, esi
-        xor ecx, ecx
-        ;mov rdi, [os_VideoBase]
-        ;mov esi, [os_Screen_Row_2]
-        add esi, edi
-        ;mov ecx, [os_Screen_Bytes]
-        rep movsb
-
-os_screen_scroll_done:
-        pop eax
-        pop ecx
-        pop edi
-        pop esi
-        ret
-; -----------------------------------------------------------------------------
-
-
-; -----------------------------------------------------------------------------
-; os_screen_clear -- Clear the screen
-;  IN:  Nothing
-; OUT:  All registers perserved
-os_screen_clear:
-        push edi
-        push ecx
-        push eax
-
-        xor ecx, ecx
-
-        ;cmp byte [os_VideoEnabled], 1
-        ;je os_screen_clear_graphics
-
-os_screen_clear_text:
-        mov ax, 0x0720          ; 0x07 for black background/white foreground, 0x20 for space (black) character
-        mov edi, 0xB8000        ; Address for start of color video memory
-        mov cx, 2000
-        rep stosw               ; Clear the screen. Store word in AX to RDI, RCX times
-        jmp os_screen_clear_done
-
-os_screen_clear_graphics:
-        ;mov rdi, [os_VideoBase]
-        xor eax, eax
-        ;mov ecx, [os_Screen_Bytes]
-        rep stosb
-
-os_screen_clear_done:
-        pop eax
-        pop ecx
-        pop edi
-        ret
-; -----------------------------------------------------------------------------
 
 
 ; =============================================================================
