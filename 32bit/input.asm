50f70a9196af4c7495d5d690aa91db8c347de428
vpn288 <vpn@vden.ru>
2015-02-17 00:20:21 +0300

    Update input.asm

--- 32bit/input.asm
+++ 32bit/input.asm
@@ -20,7 +20,7 @@ os_input:
         push edi
         push edx                        ; Counter to keep track of max accepted characters
         push eax
-
+        mov  [ curr_tlb],edi
         mov edx, ecx                    ; Max chars to accept
         xor ecx, ecx                    ; Offset from start
 
@@ -29,7 +29,9 @@ os_input_more:
         ;call os_output_char
         ;call os_dec_cursor
         call os_input_key
-        jnc os_input_halt               ; No key entered... halt until an interrupt is received
+        jnc os_input_halt
+        cmp al,5
+        je  repeat_cmnd ; No key entered... halt until an interrupt is received
         cmp al, 0x1C                    ; If Enter key pressed, finish
         je os_input_done
         cmp al, 0x0E                    ; Backspace
@@ -45,12 +47,26 @@ os_input_more:
         call os_output_char             ; Display char
         jmp os_input_more
 
+repeat_cmnd:
+        cld
+        mov     esi,prev_tlb
+        mov     edi,[curr_tlb]
+        mov     ecx,16
+        rep     movsd
+        mov     ecx,64
+      ;  int3
+        mov     esi,[curr_tlb]
+        call    os_output
+        jmp os_input_more
+
+msgrc   db  "did it again",10,13,0
+
 os_input_backspace:
         cmp ecx, 0                      ; backspace at the beginning? get a new char
         je os_input_more
-        mov al, ' '                     ; 0x20 is the character for a space
-        call os_output_char             ; Write over the last typed character with the space
-        call os_dec_cursor              ; Decrement the cursor again
+       ; mov al, ' '                     ; 0x20 is the character for a space
+        ;call os_output_char             ; Write over the last typed character with the space
+        ;call os_dec_cursor              ; Decrement the cursor again
         call os_dec_cursor              ; Decrement the cursor
         dec edi                         ; go back one in the string
         mov byte [edi], 0x00            ; NULL out the char
@@ -70,7 +86,20 @@ os_input_done:
         pop eax
         pop edx
         pop edi
+        push    edi
+        push    esi
+        push    ecx
+        mov     esi,[curr_tlb]
+        mov     edi,prev_tlb
+        mov     ecx,16
+        cld
+        rep     movsd
+        pop     ecx
+        pop     esi
+        pop     edi
         ret
+curr_tlb        dd      0
+prev_tlb        db 64    dup 0
 ; -----------------------------------------------------------------------------
 
 
