c00a97a0f24c5b693226b01d1a47716b5527881b
vpn288 <vpn@vden.ru>
2015-05-24 21:40:35 +0300

    Create blocks.asm

---
+++ minimal/blocks.asm
@@ -0,0 +1,24 @@
+
+macro alignhe20
+{ virtual
+       align 8192
+;       align 4096
+;       align 2048
+;       align 1024
+;       align 512
+        algn = $ - $$
+    end virtual
+    db algn dup 20h
+    }
+
+include "blocks\block1.asm"
+include "blocks\block2.asm"
+include "blocks\block3.asm"
+include "blocks\block4.asm"
+include "blocks\block5.asm"
+include "blocks\block6.asm"
+include "blocks\block7.asm"
+include "blocks\block8.asm"
+
+
+ ;block 9
