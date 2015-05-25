61e00bb2cde4e1283b2dc81e1b11104f49c77c85
vpn288 <vpn@vden.ru>
2015-05-25 22:31:18 +0300

    Update block1.asm

--- minimal/blocks/block1.asm
+++ minimal/blocks/block1.asm
@@ -1,60 +1,64 @@
 ;block1
- db	" TYPEZ                                                                         "
- db	" 0x 7 LOAD        0x 2 LOAD      "
- db	" 0x 3 LOAD        0x 5 LOAD      "
- db	" 0x 4 LOAD        0x 6 LOAD      "
- db	" 0x 8 LOAD        "
+ db     " TYPEZ                                                                         "
+ db     " 0x 7 LOAD        0x 2 LOAD      "
+ db     " 0x 3 LOAD        0x 5 LOAD      "
+ db     " 0x 4 LOAD        0x 6 LOAD      "
+ db     " 0x 8 LOAD        "
 
- db	" .( End of loads)  "
+ db     " .( End of loads)  "
  db " WORD: CR        0x_as_lit, 0D0A  SP@ TYPEZ ;WORD "
  db "  "
- ; db ' WORD: ifelse  If   ." Else part " Else ." Then part "  Then   ;WORD '
+ db " WORD: dotest 0x_as_lit, 1 0x_as_lit, 12   DO  CR TIMER@ 2HEX.  LOOP   NOOP ;WORD "
 
 
- db	" WORD: key "
- db	" KEY    "
- db	" Case "
- db	" DUP 0x_as_lit, 10  0x_as_lit, 19  WITHIN Of "
- db	" 0x_as_lit, 10 -  qwerty CELL+ + upper_shift_caps   0x_as_lit, A  AND + C@ SP@  TYPEZ  EndOf "
- db	" DUP  "
- db	" 0x_as_lit, 1E  0x_as_lit, 26  WITHIN  Of "
- db	" 0x_as_lit, 1E -  asdfgh CELL+ + upper_shift_caps   0x_as_lit, 9  AND + C@ SP@  TYPEZ  EndOf "
- db	" DUP  "
- db	" 0x_as_lit, 2C  0x_as_lit, 32  WITHIN Of "
- db	" 0x_as_lit, 2C -  zxcvbn CELL+ + upper_shift_caps   0x_as_lit, 7  AND + C@ SP@  TYPEZ  EndOf "
- db	" DUP  "
- db	" 0x_as_lit, 2   0x_as_lit,  D  WITHIN  Of "
- db	" 0x_as_lit,  2 -  numkys CELL+ + upper_shift_only   0x_as_lit, C  AND + C@ SP@  TYPEZ  EndOf "
- db	" DUP  "
 
- db	" 0x_as_lit, 1A  0x_as_lit, 1B  WITHIN  Of "
- db	" 0x_as_lit, 1A -  numky1 CELL+ + upper_shift_only   0x_as_lit, 2  AND + C@ SP@  TYPEZ  EndOf "
- db	" DUP  "
- db	" 0x_as_lit, 27  0x_as_lit, 29  WITHIN  Of "
- db	" 0x_as_lit, 27 -  numky2 CELL+ + upper_shift_only   0x_as_lit, 3  AND + C@ SP@  TYPEZ  EndOf "
- db	" DUP  "
+ db     " WORD: key "
+ db     " KEY    "
+ db     " Case "
+ db     " DUP 0x_as_lit, 10  0x_as_lit, 19  WITHIN Of "
+ db     " 0x_as_lit, 10 -  qwerty CELL+ + upper_shift_caps   0x_as_lit, A  AND + C@ SP@  TYPEZ  EndOf "
+ db     " DUP  "
+ db     " 0x_as_lit, 1E  0x_as_lit, 26  WITHIN  Of "
+ db     " 0x_as_lit, 1E -  asdfgh CELL+ + upper_shift_caps   0x_as_lit, 9  AND + C@ SP@  TYPEZ  EndOf "
+ db     " DUP  "
+ db     " 0x_as_lit, 2C  0x_as_lit, 32  WITHIN Of "
+ db     " 0x_as_lit, 2C -  zxcvbn CELL+ + upper_shift_caps   0x_as_lit, 7  AND + C@ SP@  TYPEZ  EndOf "
+ db     " DUP  "
+ db     " 0x_as_lit, 2   0x_as_lit,  D  WITHIN  Of "
+ db     " 0x_as_lit,  2 -  numkys  + upper_shift_only   0x_as_lit, C  AND + C@ SP@  TYPEZ  EndOf "
+ db     " DUP  "
 
- db	" 0x_as_lit, 2B  =  Of  "
- db	" 0x_as_lit, 2B -  numky3 CELL+ + upper_shift_only   0x_as_lit, 1  AND + C@ SP@  TYPEZ  EndOf  "
+ db     " 0x_as_lit, 1A  0x_as_lit, 1B  WITHIN  Of "
+ db     " 0x_as_lit, 1A -  numky1 CELL+ + upper_shift_only   0x_as_lit, 2  AND + C@ SP@  TYPEZ  EndOf "
+ db     " DUP  "
+ db     " 0x_as_lit, 27  0x_as_lit, 29  WITHIN  Of "
+ db     " 0x_as_lit, 27 -  numky2 CELL+ + upper_shift_only   0x_as_lit, 3  AND + C@ SP@  TYPEZ  EndOf "
+ db     " DUP  "
 
- db	" DUP  "
- db	" 0x_as_lit, 33  0x_as_lit, 35  WITHIN  Of "
- db	" 0x_as_lit, 33 -  numky4 CELL+ + upper_shift_only   0x_as_lit, 3  AND + C@ SP@  TYPEZ  EndOf "
- db	" DUP  "
+ db     " 0x_as_lit, 2B  =  Of  "
+ db     " 0x_as_lit, 2B -  numky3 CELL+ + upper_shift_only   0x_as_lit, 1  AND + C@ SP@  TYPEZ  EndOf  "
 
- db	" 0x_as_lit, 39  =  Of "
- db	" 0x_as_lit, 39 -  space_ CELL+ +  C@ SP@  TYPEZ  EndOf  "
- db	" EndCase Pop  ;WORD "
- db	" CR .( ----------) CR "
+ db     " DUP  "
+ db     " 0x_as_lit, 33  0x_as_lit, 35  WITHIN  Of "
+ db     " 0x_as_lit, 33 -  numky4 CELL+ + upper_shift_only   0x_as_lit, 3  AND + C@ SP@  TYPEZ  EndOf "
+ db     " DUP  "
 
-; db	 " WORD: key2 "
-; db	 ' KEY	  '
-; db	 ' Case    '
-; db	 ' DUP	0x_as_lit, 1 =	 Of  ." Escape " EndOf	 '
-; db	 ' DUP	0x_as_lit, 2 =	 Of  ." One " EndOf    '
-; db	 ' ." Another "   EndCase    ;WORD '
+ db     " 0x_as_lit, 39  =  Of "
+ db     " 0x_as_lit, 39 -  space_ CELL+ +  C@ SP@  TYPEZ  EndOf  "
+ db     ' EndCase Pop  ;WORD '
+ db     " CR .( ----------) CR "
 
- db	"  .( KEY:) key "
- db	" CR .( Here:) HERE HEX. .( Ticks:) TIMER@ 2HEX. EXIT    "
- db	0
+; db     " WORD: key2 "
+; db     ' KEY    '
+; db     ' Case    '
+; db     ' DUP  0x_as_lit, 1 =   Of  ." Escape " EndOf   '
+; db     ' DUP  0x_as_lit, 2 =   Of  ." One " EndOf    '
+; db     ' ." Another "   EndCase    ;WORD '
+
+ db     "  .( KEY:) key "
+  db " dotest "
+ db     " CR .( Here:) HERE HEX. .( Ticks:) TIMER@ 2HEX. EXIT    "
+ db     0
+;
+
  alignhe20
