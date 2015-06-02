77d8b5849f5d329d2f7a2122d54ca1feed1bc8eb
vpn288 <vpn@vden.ru>
2015-06-02 23:17:58 +0300

    Update block9.asm

--- minimal/blocks/block9.asm
+++ minimal/blocks/block9.asm
@@ -17,22 +17,27 @@ db " ' chr ' BADWORD CELL+ ! "
 db " FORTH32 CONTEXT ! "
 db " span's UNLINK "
 
- db " WORD: span          OVER OVER SWAP- + 1+ DO    BL WORD  span's SFIND EXECUTE  SP@ TYPEZ B, LOOP  Pop Pop  ;WORD "
+ db " WORD: (span)          OVER OVER SWAP- + 1+ DO    BL WORD  span's SFIND EXECUTE  SP@ TYPEZ B, LOOP  ;WORD "
+ db ' WORD: span            OVER OVER LIT, LIT,      (span)   ;WORD '
+ db " WORD: layout:           [ ', WORD: ]    ;WORD "
+ db " WORD: ;layout           [ ', ;WORD ]    ;WORD "
 
-;db " layout: eng                      "
+ ;db " WORD: eng                      "
 ;db "           upper_shift_caps "
- db "          HERE  .( before ) SP@ HEX.  0x 10 0x 19 span  q w e r t y u i o p  Q W E R T Y U I O P SP@ HEX.   "
- db "                0x 1E 0x 26 span  a s d f g h j k l    A S D F G H J K L "
- db "                0x 2C 0x 32 span  z x c v b n m        Z X C V B N M "
+ db '          HERE  .( before ) SP@ HEX. 0x 10  0x 19 span  q w e r t y u i o p  Q W E R T Y U I O P SP@ HEX.   '
+ db "        stop         0x 1E  0x 26 span  a s d f g h j k l    A S D F G H J K L "
+ db "                 0x 2C  0x 32 span  z x c v b n m        Z X C V B N M "
 
 ;db "          upper_shift_only "
- db "                0x 02 0x 0D span 1 2 3 4 5 6 7 8 9 0 - =  ! @ # $ % ^ & * ( ) _ + "
- db "                0x 1A 0x 1B span [ ] { } "
- db "                0x 27 0x 29 span ; ' `   : QUOTE  ~ "
- db "                0x 2B 0x 2B span  \ | "
- db "                0x 33 0x 35 span , . / < > ? "
- db "                0x 39 0x 39  span  BL BL .( after) SP@ HEX.   TYPEZ "
 
+ db "                 0x 02  0x 0D span 1 2 3 4 5 6 7 8 9 0 - =  ! @ # $ % ^ & * ( ) _ + "
+ db "                 0x 1A  0x 1B span [ ] { } "
+ db "                 0x 27  0x 29 span ; ' `   : QUOTE  ~ "
+ db "                 0x 2B  0x 2B span  \ | "
+ db "                 0x 33  0x 35 span , . / < > ? "
+ db '                 0x 39  0x 39  span  BL BL .( after) SP@ HEX.   TYPEZ '
+ ;db " ;WORD "
+ ;db " eng "
 ;db " layout: rus_win1251 "
 ;db "         upper_shift_caps "
 ;db "                0x 10 0x 1B span é ö ó ê å í ã ø ù ç õ ú  É Ö Ó Ê Å Í Ã Ø Ù Ç Õ Ú "
