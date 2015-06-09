6f7c6f1c488454da4f224b0fa3ca4734283f7b75
vpn288 <vpn@vden.ru>
2015-06-09 13:09:55 +0300

    Update block9.asm

--- minimal/blocks/block9.asm
+++ minimal/blocks/block9.asm
@@ -18,57 +18,51 @@ db " span's FORTH32 LINK    span's CONTEXT ! "
 db " ' chr ' BADWORD CELL+ ! "
 db " FORTH32 CONTEXT ! "
 db " span's UNLINK "
-; db " WORD: span             COMPILE BRANCH HERE COMPILE 0  >R OVER OVER SWAP- + 1+ DO    BL WORD  span's SFIND EXECUTE SP@ TYPEZ B, LOOP R>  HERE  CELL- SWAP!    ;WORD "
+
 db " VECTOR upper? "
 
-db " ' upper_shift_caps  ' upper? CELL+ ! "
+db " WORD:  caps_shift   [ ' upper_shift_caps  LIT,  ' upper? CELL+ LIT, ] ! ;WORD "
+db " WORD:  only_shift   [ ' upper_shift_only  LIT,  ' upper? CELL+ LIT, ] ! ;WORD "
 
 db ' WORD: ;span  >R WITHIN R> SWAP   0 = If  >R >R  DUP   R@ 0x_as_lit, 4 CELLs + @ -  R> 0x_as_lit, B CELLs + + R> upper? AND + C@ SP@ TYPEZ Pop  Else Pop Pop Then   DUP ;WORD '
- db " IMMEDIATES CURRENT ! "
- db " WORD: HEX,  [ ' 0x_as_lit, LIT, ] , ;WORD "
- ;db " WORD: (span)             OVER LIT, DUP LIT, COMPILE WITHIN  COMPILE HEX. [ ' Of LIT, ] ,   COMPILE HERE span [ ' EndOf LIT, ] ,     ;WORD "
- db " WORD: span:             HERE LIT, COMPILE SWAP OVER LIT, DUP LIT, OVER OVER SWAP- 1+ LIT,   COMPILE BRANCH HERE COMPILE 0  HERE HEX. >R OVER OVER SWAP- + 1+ DO    BL WORD  span's SFIND EXECUTE SP@ TYPEZ B, LOOP R>  HERE  CELL- SWAP!   COMPILE ;span  ;WORD "
- db " FORTH32 CURRENT !    "
+db " IMMEDIATES CURRENT ! "
+db " WORD: HEX,  [ ' 0x_as_lit, LIT, ] , ;WORD "
 
-
- db " .( tessts ) "
-
-
-; db " WORD: tesst    [ 0x 10 0x 19 ]   span:  q w e r t y u i o p  Q W E R T Y U I O P  ;span   ;WORD "
-
-
-; db " 0x 11  DUP  tesst .( tersts )  "
+db " WORD: span:             HERE LIT, COMPILE SWAP OVER LIT, DUP LIT, OVER OVER SWAP- 1+ LIT,   COMPILE BRANCH HERE COMPILE 0  HERE HEX. >R OVER OVER SWAP- + 1+ DO    BL WORD  span's SFIND EXECUTE SP@ TYPEZ B, LOOP R>  HERE  CELL- SWAP!   COMPILE ;span  ;WORD "
+db " FORTH32 CURRENT !    "
 
  ;db " WORD: layout:           [ ', WORD: ]    ;WORD "
  ;db " WORD: ;layout           [ ', ;WORD ]    ;WORD "
 
- db " WORD: eng                      "
-;db "           upper_shift_caps "
- db '              [ 0x 10 0x 19 ]  span:  q w e r t y u i o p  Q W E R T Y U I O P   '
- db "              [ 0x 1E 0x 26 ]  span:  a s d f g h j k l    A S D F G H J K L     "
- db "              [ 0x 2C 0x 32 ]  span:  z x c v b n m        Z X C V B N M        "
+ db " WORD: eng            "
+ db "           caps_shift "
+ db "              [ 0x 10  0x 19 ]  span:  q w e r t y u i o p  Q W E R T Y U I O P   "
+ db "              [ 0x 1E  0x 26 ]  span:  a s d f g h j k l    A S D F G H J K L     "
+ db "              [ 0x 2C  0x 32 ]  span:  z x c v b n m        Z X C V B N M         "
 
-;db "          upper_shift_only "
+ db "           only_shift "
 
-; db "                 0x 02  0x 0D span 1 2 3 4 5 6 7 8 9 0 - =  ! @ # $ % ^ & * ( ) _ + "
-; db "                 0x 1A  0x 1B span [ ] { } "
-; db "                 0x 27  0x 29 span ; ' `   : QUOTE  ~ "
-; db "                 0x 2B  0x 2B span  \ | "
-; db "                 0x 33  0x 35 span , . / < > ? "
-; db '                 0x 39  0x 39  span  BL BL .( after) SP@ HEX.   TYPEZ '
+ db "              [ 0x 02  0x 0D ] span: 1 2 3 4 5 6 7 8 9 0 - =  ! @ # $ % ^ & * ( ) _ + "
+ db "              [ 0x 1A  0x 1B ] span: [ ] { }            "
+ db "              [ 0x 27  0x 29 ] span: ; ' `   : QUOTE  ~ "
+ db "              [ 0x 2B  0x 2B ] span:  \ |               "
+ db "              [ 0x 33  0x 35 ] span: , . / < > ?        "
+ db "              [ 0x 39  0x 39 ] span:  BL BL             "
  db " ;WORD "
 
-; db " 0x 13  0x 10  0x 19 span "
-;db " layout: rus_win1251 "
-;db "         upper_shift_caps "
-;db "                0x 10 0x 1B span é ö ó ê å í ã ø ù ç õ ú  É Ö Ó Ê Å Í Ã Ø Ù Ç Õ Ú "
-;db "                0x 1E 0x 29 span ô û â à ï ð î ë ä æ ý ¸  Ô Û Â À Ï Ð Î Ë Ä Æ Ý ¨ "
-;db "                0x 2C 0x 34 span ÿ ÷ ñ ì è ò ü á þ        ß × Ñ Ì È Ò Ü Á Þ "
-;db "         upper_shift_only "
-;db "            0x 02 0x 0D span 1 2 3 4 5 6 7 8 9 0 - =  ! QUOTE ¹ ; % : ? * ( ) _ + "
-;db "                0x 35 0x 35 span . , "
-;db "                0x 39 0x 39 span BL BL "
-;db " 0x 11 DUP eng "
+
+db " WORD: rus_win1251 "
+db "          caps_shift "
+db "              [  0x 10 0x 1B ] span: é ö ó ê å í ã ø ù ç õ ú  É Ö Ó Ê Å Í Ã Ø Ù Ç Õ Ú "
+db "              [  0x 1E 0x 29 ] span: ô û â à ï ð î ë ä æ ý ¸  Ô Û Â À Ï Ð Î Ë Ä Æ Ý ¨ "
+db "              [  0x 2C 0x 34 ] span: ÿ ÷ ñ ì è ò ü á þ        ß × Ñ Ì È Ò Ü Á Þ "
+db "         only_shift "
+db "              [ 0x 02 0x 0D ] span: 1 2 3 4 5 6 7 8 9 0 - =  ! QUOTE ¹ ; % : ? * ( ) _ + "
+db "              [ 0x 35 0x 35 ] span: . , "
+db "              [ 0x 39 0x 39 ] span: BL BL  "
+db " ;WORD "
+
+
 db " EXIT "
 db     0
  alignhe20
