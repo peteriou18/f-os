751c8bf1252a17a30f00807a530ab584123a6132
vpn288 <vpn@vden.ru>
2015-06-11 00:05:54 +0300

    Update block1.asm

--- minimal/blocks/block1.asm
+++ minimal/blocks/block1.asm
@@ -6,14 +6,15 @@
  db     " 0x 8 LOAD        0x 9 LOAD      "
 
  db     " .( End of loads)  "
- db " WORD: CR        0x_as_lit, 0D0A  SP@ TYPEZ ;WORD "
+
+
 
 
  db     " WORD: key "
  db     " KEY  DUP  eng ;WORD "
 
- db     " WORD: EXPECT          Begin key  Again ;WORD "
- db     " CR .( OK>) EXPECT "
+; db     " WORD: EXPECT         0x_as_lit, 2020202020202020  0x_as_lit, 2000 ! 0x_as_lit, 1FFF  Begin  1+ DUP  key   SWAP  C! DUP  C@ DUP HEX.   0x_as_lit, 1C = Until  0x_as_lit, 2000 TYPEZ ;WORD "
+; db     " CR 0x 40 0x B8010 C!  0x B8010 C@ HEX.    CR .( OK>) EXPECT  0 >IN ! INTERPRET  "
 
  db     " CR .( ----------) CR "
 
