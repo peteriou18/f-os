e5fef69845d0c19c72d80bf94e8dfb78377e9449
vpn288 <vpn@vden.ru>
2015-06-10 09:31:43 +0300

    Update block1.asm

--- minimal/blocks/block1.asm
+++ minimal/blocks/block1.asm
@@ -10,39 +10,11 @@
 
 
  db     " WORD: key "
- db     " KEY    "
- db     " Case "
- db     " DUP 0x_as_lit, 10  0x_as_lit, 19  WITHIN Of "
- db     " 0x_as_lit, 10 -  qwerty CELL+ + upper_shift_caps   0x_as_lit, A  AND + C@ SP@  TYPEZ  EndOf "
- db     " DUP  "
- db     " 0x_as_lit, 1E  0x_as_lit, 26  WITHIN  Of "
- db     " 0x_as_lit, 1E -  asdfgh CELL+ + upper_shift_caps   0x_as_lit, 9  AND + C@ SP@  TYPEZ  EndOf "
- db     " DUP  "
- db     " 0x_as_lit, 2C  0x_as_lit, 32  WITHIN Of "
- db     " 0x_as_lit, 2C -  zxcvbn CELL+ + upper_shift_caps   0x_as_lit, 7  AND + C@ SP@  TYPEZ  EndOf "
- db     " DUP  "
- db     " 0x_as_lit, 2   0x_as_lit,  D  WITHIN  Of "
- db     " 0x_as_lit,  2 -  numkys  + upper_shift_only   0x_as_lit, C  AND + C@ SP@  TYPEZ  EndOf "
- db     " DUP  "
+ db     " KEY  DUP  eng ;WORD "
 
- db     " 0x_as_lit, 1A  0x_as_lit, 1B  WITHIN  Of "
- db     " 0x_as_lit, 1A -  numky1 CELL+ + upper_shift_only   0x_as_lit, 2  AND + C@ SP@  TYPEZ  EndOf "
- db     " DUP  "
- db     " 0x_as_lit, 27  0x_as_lit, 29  WITHIN  Of "
- db     " 0x_as_lit, 27 -  numky2 CELL+ + upper_shift_only   0x_as_lit, 3  AND + C@ SP@  TYPEZ  EndOf "
- db     " DUP  "
+ db     " WORD: EXPECT          Begin key  Again ;WORD "
+ db     " CR .( OK>) EXPECT "
 
- db     " 0x_as_lit, 2B  =  Of  "
- db     " 0x_as_lit, 2B -  numky3 CELL+ + upper_shift_only   0x_as_lit, 1  AND + C@ SP@  TYPEZ  EndOf  "
-
- db     " DUP  "
- db     " 0x_as_lit, 33  0x_as_lit, 35  WITHIN  Of "
- db     " 0x_as_lit, 33 -  numky4 CELL+ + upper_shift_only   0x_as_lit, 3  AND + C@ SP@  TYPEZ  EndOf "
- db     " DUP  "
-
- db     " 0x_as_lit, 39  =  Of "
- db     " 0x_as_lit, 39 -  space_ CELL+ +  C@ SP@  TYPEZ  EndOf  "
- db     ' EndCase Pop  ;WORD '
  db     " CR .( ----------) CR "
 
 
