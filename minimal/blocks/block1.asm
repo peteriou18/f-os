bd3ef2bfeb30e1c03baa36b1230326d636bcdc1b
vpn288 <vpn@vden.ru>
2015-06-18 14:31:15 +0300

    Update block1.asm

--- minimal/blocks/block1.asm
+++ minimal/blocks/block1.asm
@@ -8,7 +8,7 @@
  db     " .( End of loads)   "
 
  db     "  FORTH32 CURRENT ! FORTH32 CONTEXT ! "
- db     '  WORD: badw      ." Very bad Word:" HERE 1+ TYPEZ ;WORD '
+ db     '  WORD: badw      ." Very bad Word:" HERE 1+ TYPEZ CR ;WORD '
  db     " ' badw ' BADWORD CELL+ ! "
 
  db     "  0x  20202020 CONSTANT 4Spaces "
@@ -18,7 +18,7 @@
  db     "  VARIABLE tib#  tibsize 0x 10 *  ALLOT     "
  db     "  WORD: tib      tib# tib$  @ +   ;WORD "
  ;db     "  4Spaces fixframe tib DUP 1st SWAP! DUP CELL+ 1st  SWAP!  DUP CELL+ 1st  SWAP! DUP CELL+ 1st  SWAP! DUP CELL+ 1st  SWAP! "
- db      " tib HEX.   tib @ HEX.    'exit' tib !   tib @ HEX. "
+; db      " tib HEX.   tib @ HEX.    'exit' tib !   tib @ HEX. "
  db     " WORD: test5      fix_frame  5th HEX. 4th HEX. 3rd HEX. 2nd HEX. 1st HEX. ;WORD "
 
 
@@ -27,27 +27,27 @@
 
  db     " WORD: do_backspace  1- 1- hex, 082008 SP@ TYPEZ  Pop  ;WORD   "
  db     " WORD: mask          hex, 3C0 AND  ;WORD "
- db     " WORD: prev_cmd      tib$ @ tibsize - mask  tib$ !   ;WORD "
+ db     " WORD: prev_cmd      tib$ @ tibsize - mask  tib$ ! tib C@  BLOCK CELL+ !  ;WORD "
  db     " WORD: next_cmd      tib$ @ tibsize + mask  tib$ !   ;WORD "
  db     " WORD: do_enter      DUP 4spaces SWAP! CELL+ DUP 'exit' SWAP! CELL+ DUP 4spaces SWAP! CELL+  0 SWAP!  ;WORD "
 
  db     " WORD: ?do   Case           "
- db     '                   DUP      hex, 1C00 = Of    Pop tib - BLOCK CELL+ !  next_cmd   Pop  -1   EndOf  '
+ db     '                   DUP      hex, 1C00 = Of    Pop tib  - DUP BLOCK CELL+ ! tib C!  next_cmd   Pop  -1   EndOf  '
  db     "                   DUP      hex, E00  = Of    Pop Pop do_backspace  0     EndOf  "
- db     '                   DUP      hex, 4800 = Of    ."  BACK " prev_cmd hex, 0D SP@ TYPEZ Pop Pop  tib TYPEZ 0 EndOf '
+ db     '                   DUP      hex, 4800 = Of    Pop HEX. prev_cmd  tib HEX. tib tib C@ + DUP HEX. tib 1+ TYPEZ Pop 0 EndOf '
  db     "             SWAP C! 0   "
  db     ' EndCase      ;WORD '
 
- db     " WORD: EXPECT    tib 1-        "
+ db     " WORD: EXPECT    tib      "
  db     '                 Begin  1+ DUP   key    ?do   Until      '
  db     "                 ;WORD "
 
  db     " WORD: set_console_input  0 BLOCK ! tibsize BLOCK CELL+ !  "
- db     "                        tib BLOCK CELL+ CELL+ !               ;WORD "
+ db     "                        tib 1+ BLOCK CELL+ CELL+ !               ;WORD "
 
  db     ' WORD: F-SYSTEM  '
- db     '                 Begin set_console_input CR  tib TYPEZ CR ." SP:" SP@ HEX. ." HERE:" HERE HEX. ." TICKs:" TIMER@ 2HEX.  '
- db     '                 ."  OK>" EXPECT   CR 0 >IN ! INTERPRET Again ;WORD '
+ db     '                 Begin set_console_input CR  tib 1+ TYPEZ CR ." SP:" SP@ HEX. ." HERE:" HERE HEX. ." TICKs:" TIMER@ 2HEX.  '
+ db     '                 CR ." OK>" EXPECT   CR 0 >IN ! INTERPRET Again ;WORD '
 
  db     "   F-SYSTEM   "
 
