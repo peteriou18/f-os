4a39f95c0d463ae5fd88c9436152063e0aa34e85
vpn288 <vpn@vden.ru>
2015-07-17 20:53:19 +0300

    Update blocka.asm

--- minimal/blocks/blocka.asm
+++ minimal/blocks/blocka.asm
@@ -9,7 +9,16 @@ db " editor CURRENT ! "
  db " ASSEMBLER FORTH32 LINK    "
  db " ASSEMBLER CONTEXT !    "
 
-
+db " HEADER symb HERE CELL+ , "
+db " mov_edx,# ' Pop @ , call_edx "
+db " mov_ebp,eax "
+db " call_edx     "
+db " mov_ecx,eax  "
+db " mov_eax,ebp      "
+db " mov_ch,# 0x 1F B, "
+db " mov_[gs:eax],cx "
+db " ret "
+db " ALIGN    "
 
 db " HEADER Left_upper HERE CELL+ , "
 db " mov_edx,# ' Pop @ , call_edx "
@@ -28,6 +37,8 @@ db " mov_edx,# ' Pop @ , call_edx "
 db " mov_w[gs:eax],# 0x C8 B, 0x 1F B, "
 db " ret "
 db " ALIGN    "
+
+
 
 db " HEADER Right_lower HERE CELL+ , "
 db " mov_edx,# ' Pop @ , call_edx "
@@ -64,6 +75,16 @@ db " ALIGN    "
 
 db " editor FORTH32 LINK   editor CONTEXT ! "
 
+db " CREATE (win)       0 , 0 , 0 ,    "
+db " WORD: win          (win) @ (win) CELL+ @ (win) CELL+ CELL+ @ ;WORD "
+
+db " WORD: xy-adr   (( x y -- addr ) hex, A0 * SWAP 2* + ;WORD "
+
+db " WORD: win_point    xy-adr (win)        ;WORD "
+db " WORD: win_width    (win)  CELL+        ;WORD "
+db " WORD: win_height   (win)  CELL+ CELL+  ;WORD "
+
+
 db ' WORD: corners       3rd  Left_upper '
 db "                     3rd 2nd 2* +  Right_upper  "
 db "                     3rd 1st hex, A0 * +  Left_lower   "
@@ -74,16 +95,20 @@ db "              3rd 1st hex, A0 * + 2nd Hline "
 db "              3rd 1st Vline "
 db "              3rd 2nd 2* + 1st Vline ;WORD "
 
-db " WORD: border  hex, 0A8  hex, 8   hex, 3 (( address of Left upper corner width height ) "
+db " WORD: border   (( address of Left upper corner width height ) "
 db "               fix_frame  sides corners ;WORD "
 
-db " WORD: DRAW border ;WORD "
-db " WORD: KEY  NOOP ;WORD "
+db " WORD: fill  hex, 4000 C@   hex, 1 hex, 1 xy-adr fix_frame (( char addr_to ) "
+db "             0 win_width @ 1- 1- Do 2nd 1st symb hex, 1 2nd+ hex, 2 1st+ Loop Pop Pop ;WORD  "
 
+db " WORD: DRAW   border fill ;WORD "
+db " WORD: key  KEY eng ;WORD "
+db " WORD: ?Esc hex, 100 = ;WORD "
 
 
 db " FORTH32 CURRENT !  "
-db ' WORD: EDIT  ." editor "  Begin DRAW KEY -1 Until  ;WORD '
+db " 0x 0  0x 0 win_point !  0x 48 win_width !  0x 14 win_height ! "
+db ' WORD: EDIT  ." editor "  Begin win DRAW key ?Esc Until  ;WORD '
 db " FORTH32 CONTEXT ! "
 db " EXIT "
 
